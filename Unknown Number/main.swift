a5471153a4bec632773fae7fd3c2539968507e08
devGod <devgod@mail.ru>
2021-02-27 19:03:51 +0300

    Игра угадай число

--- Unknown Number/main.swift
+++ Unknown Number/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
-print("Hello, World!")
+let randomNumber = UInt8.random(in: 1...250)
+print("Введите число от 1 до 250 и нажмите Enter")
+var isWin = false
 
+repeat {
+    guard let userNumber = UInt8(readLine() ?? "") else {
+        print("Вы ввели некорректное число. Попробуйте снова")
+        continue
+    }
+    
+    if userNumber < randomNumber {
+        print("Число больше, чем то что вы ввели")
+    } else if userNumber > randomNumber {
+        print("Число меньше, чем то что вы ввели")
+    } else {
+        print("Вы угадали!")
+        isWin = true
+    }
+} while !isWin
